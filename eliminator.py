ec9e427187f5fd9238180572c9ebaf1a4e72ffab
FBonnefoy <48140285+FBonnefoy@users.noreply.github.com>
2019-03-02 00:37:54 +0100

    Update eliminator.py

--- eliminator.py
+++ eliminator.py
@@ -1,49 +1,65 @@
 import os
-path = input('SVP introduire directoire du travail:\n')
+path = input('Enter CWD please :\n')
 if len(path)<1:
-	os.chdir('C:\\Users\\f.bonnefoy\\Desktop\\Brands')
+	cwd = os.getcwd()
 else:
-	os.chdir(path)
-fname=input("file name of list?\n")
-fhandle=open(fname)
+	while path!="q":
+		try:
+			os.chdir(path)
+		except:
+			path=input("Invalid path\nIf you want to quit, then please write 'q'\nRemember to write windows' folder paths with the following format: C:\\xxx\\xxx\\xxx")
+			continue
+			
 list=[]
+fname=None
+fname2=None
+ofname=None
+
+while fname!="q":
+	try:
+		fname=input("Please enter name of text file for keys with right values:\n)
+		fhandle=open(fname)
+	except:
+		print("Invalid file name. Remember to put the '.txt' suffix at the end of the file\nYou can exit by typing 'q' in the terminal.")
+		continue
+
 for line in fhandle:
 	word=line.strip()
-	word=str(word)
 	if word not in list:
 		list.append(word)
-str1 = ','.join(str(e) for e in list)
-str1 = str1.replace("'",'')
-str1 = str1.replace("[",'')
-str1 = str1.replace("]",'')
-print(list)
-print(str1)
-rlist=str.split(',')
+fhandle.close()
+			    
+while fname2!="q":
+	try:
+		fname2=input("Please enter name of text file for list with values to be corrected:\n) 
+		fhandle2=open(fname2)
+	except:
+		print("Invalid file name. Remember to put the '.txt' suffix at the end of the file\nYou can exit by typing 'q' in the terminal.")
+		continue
+
+
+
 vlist=[]
-fname2=input("file name of inputs?\n")
-fhandle2=open(fname2)
-ofname=input("name of output")
-of=open(ofname,"w+")
 for line in fhandle2:
-	word2=line.strip()
-	if word not in list:
-		vlist.append("yes")
-		of.write("yes\n")
-	else:
-		vlist.append("no")
-		of.write("no\n")
+	word=line.strip()
+	if word not in vlist:
+		vlist.append(word)
+rlist=[]
+
+while ofname!="q":
+	try:
+		ofname=input("Please enter the name that you want for the output text file.\nRemember to put the '.txt' suffix at the end of the name.)
+ 		of=open(ofname,"w+")
+	except:
+		print("Invalid file name. Remember to put the '.txt' suffix at the end of the file\nYou can exit by typing 'q' in the terminal.")
+		continue
+
+for i in range(len(vlist)):
+		if vlist[i] in list:
+			rlist.append("yes")
+			of.write("yes\n")
+		else:
+			rlist.append("no")
+			of.write("no\n")
+print(rlist)
 of.close()
-for i in vlist:
-	print(i)
-str2 = ','.join(str(e) for e in list)
-str2 = str2.replace("'",'')
-str2 = str2.replace("[",'')
-str2 = str2.replace("]",'')
-cont=input('continue?\n')
-if len(cont)<1:
-	sys.exit()
-else:
-	outfile=input("name of output")
-	brandput = open(outfile,"w")
-	brandput.write(str2)
-	brandput.close()
